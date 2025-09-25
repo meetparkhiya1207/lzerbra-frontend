6e857741d9029112908ea50c2ff699b2e5ae0515
meetparkhiya1207 <developermeet1207@gmail.com>
2025-09-25 08:53:52 +0530

    live count add changes

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.DEV
-  ? "http://localhost:5000"
-  : "https://api.yourdomain.com"; // deploy url
+const URL = "https://lzebra-backend-2.onrender.com"; // deploy url
 
 const globalAny = window;
 if (!globalAny.__SOCKET__) {
